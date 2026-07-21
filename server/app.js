44a7979bb613d8113eb5e050e12f8035b1d9613a
o2postspace <75207334+o2postspace@users.noreply.github.com>
2026-07-22 02:30:39 +0900

    fix: restore ad-free notice reading

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 // API 라우트
 app.use("/api/notices", require("./routes/notices"));
+app.use("/api/notice-content", require("./routes/notice-content"));
 app.use("/api/schedules", require("./routes/schedules"));
 app.use("/api/hot-notices", require("./routes/hot-notices"));
 app.use("/api/updates", require("./routes/updates"));
